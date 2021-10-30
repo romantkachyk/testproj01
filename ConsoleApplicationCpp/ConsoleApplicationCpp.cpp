6fe29a64c302058881c1f925cadb1890755a4791
v-rtkachyk <v-rtkachyk@microsoft.com>
2021-10-31 00:06:20 +0300

    Initial commit

---
+++ ConsoleApplicationCpp/ConsoleApplicationCpp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a = 0;
+
+	cout << "Введите любое 4-ех значное число: ";
+	cin >> a;
+	a = (a - (a / 100) * 100) / 10;
+	cout << "Результат: " << a << endl;
+}
